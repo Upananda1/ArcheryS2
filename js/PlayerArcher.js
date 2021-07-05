53443c456f32d73f15021ed9f2eeb9cbf5a8d7e8
Upananda1 <83901952+Upananda1@users.noreply.github.com>
2021-07-05 18:32:43 +0530

    Add files via upload

---
+++ js/PlayerArcher.js
@@ -0,0 +1,43 @@
+class PlayerArcher {
+    constructor(x,y,width,height){
+         var options = {
+        isStatic: true
+      };
+
+      this.body = Bodies.rectangle(x, y, width, height, options);
+
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("./assets/playerArcher.png");
+
+      Matter.Body.setAngle(this.body, -PI/2);
+    
+      World.add(world, this.body)
+
+    }
+
+  display(){
+    var pos = this.body.position;
+    var angle = this.body.angle;
+
+    if (keyIsDown(RIGHT_ARROW) && angle < -1.2) {
+      angle += 0.02;
+      Matter.Body.setAngle(this.body, angle)
+    }
+
+    if (keyIsDown(LEFT_ARROW) && angle > -1.9) {
+      angle -= 0.02;
+      Matter.Body.setAngle(this.body, angle)
+    }
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+
+  
+    
+}
